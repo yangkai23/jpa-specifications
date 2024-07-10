52c862cc7df1eeb363b8ddad4a02c00a7964cb38
Shanmukha Anirudh <tallurishanmukha.anirudh@clarivate.com>
2024-07-10 07:36:00 +0530

    added cors mapping for frontend

---
+++ src/main/java/com/clvt/springJpaSpecification/config.java
@@ -0,0 +1,21 @@
+package com.clvt.springJpaSpecification;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class config {
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping(("/**"))
+                        .allowedMethods("*")
+                        .allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
+}
